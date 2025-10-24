8bb0c6a453ff39a88ed620c4fa5b476836f725ed
usamazafarawan <usamazafar111@yahoo.com>
2025-10-24 22:39:16 +0500

    update

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,10 @@ const cors = require("cors");
 export const app = express();
 const server = http.createServer(app);
 
- app.use(express.json({limit: '50mb'}));
-  app.use(express.urlencoded({limit: '50mb',     extended:true  },));
-app.use(bodyParser.json({limit: '50mb'}));
+ app.use(express.json({limit: '200mb'}));
+ app.use(express.urlencoded({limit: '200mb',     extended:true  },));
+ app.use(bodyParser.json({limit: '200mb'}));
+ app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 
 
 app.use(cors(
