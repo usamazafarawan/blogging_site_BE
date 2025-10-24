65562d569345260ad03aae9f172b11edc00f924d
usamazafarawan <usamazafar111@yahoo.com>
2025-10-24 22:31:04 +0500

    set env

--- src/app.ts
+++ src/app.ts
@@ -97,10 +97,10 @@ export { gfsBucket };
 
  const environment = {
     // local backend URL
-   //  apiUrl:  'http://localhost:3000/api'
+  //   apiUrl:  'http://localhost:3000/api'
 
       // Live backend URL
-    apiUrl:  'https://blogging-site-be.vercel.app/api'
+   apiUrl:  'https://blogging-site-be.vercel.app'
 };
 
 
